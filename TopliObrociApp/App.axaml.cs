b00d778179739c0f4cc9c84adc08295d383a8d4f
Abdullah Kurgas <abdullahkurgas07@gmail.com>
2026-09-23 13:50:43 +0200

    Change splash screen design

--- TopliObrociApp/App.axaml.cs
+++ TopliObrociApp/App.axaml.cs
@@ -1,6 +1,8 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using TopliObrociApp.Configuration;
 using TopliObrociApp.Windows;
 
 namespace TopliObrociApp;
@@ -20,23 +22,23 @@ public class App : Application
             desktop.MainWindow = splashScreen;
             splashScreen.Show();
 
-            // try
-            // {
-            //     var mounted = await AppSettings.EnsureUsersShareMountedAsync();
-            //
-            //     if (!mounted) throw new Exception("Nije dohvacena putanja ka users.json");
-            // }
-            // catch (Exception e)
-            // {
-            //     splashScreen.Close();
-            //     return;
-            // }
-            //
-            // var mainWindow = new MainWindow();
-            // desktop.MainWindow = mainWindow;
-            // mainWindow.Show();
-            //
-            // splashScreen.Close();
+            try
+            {
+                var mounted = await AppSettings.EnsureUsersShareMountedAsync();
+
+                if (!mounted) throw new Exception("Nije dohvacena putanja ka users.json");
+            }
+            catch (Exception e)
+            {
+                splashScreen.Close();
+                return;
+            }
+
+            var mainWindow = new MainWindow();
+            desktop.MainWindow = mainWindow;
+            mainWindow.Show();
+
+            splashScreen.Close();
         }
 
         base.OnFrameworkInitializationCompleted();
